654664b12618b8eb42d872e5254b62cd4794923e
Kytnacode <kytnacode@gmail.com>
2024-01-27 16:44:50 -0700

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,358 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- UI
+  "j-hui/fidget.nvim",
+  "dnlhc/glance.nvim",
+  "stevearc/dressing.nvim",
+  "nvim-telescope/telescope.nvim",
+  "junegunn/fzf.vim",
+  "ray-x/navigator.lua",
+  "nvim-lualine/lualine.nvim",
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup()
+    end,
+  },
+  {
+    "kosayoda/nvim-lightbulb",
+    config = function()
+      require("nvim-lightbulb").setup({
+        autocmd = { enabled = true },
+      })
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+  {
+    "linrongbin16/lsp-progress.nvim",
+    config = function() -- setup on load
+      require("lsp-progress").setup({})
+    end,
+  },
+  {
+    "Wansmer/symbol-usage.nvim",
+    event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  -- Config Util
+  {
+    "folke/neodev.nvim",
+    opts = {}
+    --config = function()
+    --  require("neodev").setup({
+    --    override = function(_, library)
+    --      library.enabled = true
+    --      library.plugins = true
+    --    end,
+    --  })
+    --end,
+  }, -- TODO: fix or remove this plugin, does not work as expected
+
+  -- Editor
+  "b0o/schemastore.nvim",
+  "nvim-treesitter/nvim-treesitter",
+  {
+    "soulis-1256/hoverhints.nvim",
+    config = function()
+      require("hoverhints").setup({})
+    end
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "hinell/lsp-timeout.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+  },
+
+  -- Language Server Protocol (LSP), DAP (Debuger Adapter Protocol), Linters, Formatters and autocompletion
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/nvim-cmp",
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  }, -- install Language Servers (ls), DA (Debuger Adapters), linters and formatters seamlessly
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup()
+    end,
+  },
+  "neovim/nvim-lspconfig",
+  "RishabhRD/nvim-lsputils",
+  "mfussenegger/nvim-dap",
+  "rcarriga/nvim-dap-ui",
+  "mfussenegger/nvim-lint",
+  "mhartington/formatter.nvim",
+
+  "nvim-lua/plenary.nvim", -- TODO: mark this as a telescope dependency
+}, {})
+
+-- clipboard --
+vim.o.clipboard = "unnamedplus"
+
+-- Glance Keymaps
+vim.keymap.set("n", "gD", "<CMD>Glance definitions<CR>")
+vim.keymap.set("n", "gR", "<CMD>Glance references<CR>")
+vim.keymap.set("n", "gY", "<CMD>Glance type_definitions<CR>")
+vim.keymap.set("n", "gM", "<CMD>Glance implementations<CR>")
+
+-- Trouble keymaps
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
+-- LSP Keymaps
+vim.keymap.set({ "n", "i" }, "<C-space>", vim.lsp.buf.code_action)
+
+-- Lua line --
+require("lualine").setup({
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "filename" },
+    lualine_c = {
+      -- invoke `progress` here.
+      require("lsp-progress").progress,
+    },
+  },
+  options = {
+    theme = "catppuccin-mocha", -- set lualine theme, commonly the same set in catppucin theme config
+  },
+})
+
+-- listen lsp-progress event and refresh lualine
+vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+  group = "lualine_augroup",
+  pattern = "LspProgressStatusUpdated",
+  callback = require("lualine").refresh,
+})
+
+-- Get highlight group
+local function h(name)
+  return vim.api.nvim_get_hl(0, { name = name })
+end
+
+-- hl-groups can have any name
+vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("StatusLine").bg, italic = true })
+vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("StatusLine").bg, fg = h("Comment").fg, italic = true })
+vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, bg = h("StatusLine").bg, italic = true })
+vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, bg = h("StatusLine").bg, italic = true })
+vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("StatusLine").bg, italic = true })
+
+-- Set symbolusage format
+-- symbol usage is the plugin that show how many usages has a symbol (function, var, etc) upon it
+local function text_format(symbol)
+  local res = {}
+
+  local round_start = { "", "SymbolUsageRounding" }
+  local round_end = { "", "SymbolUsageRounding" }
+
+  if symbol.references then
+    local usage = symbol.references <= 1 and "usage" or "usages"
+    local num = symbol.references == 0 and "no" or symbol.references
+    table.insert(res, round_start)
+    table.insert(res, { "󰌹 ", "SymbolUsageRef" })
+    table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
+    table.insert(res, round_end)
+  end
+
+  if symbol.definition then
+    if #res > 0 then
+      table.insert(res, { " ", "NonText" })
+    end
+    table.insert(res, round_start)
+    table.insert(res, { "󰳽 ", "SymbolUsageDef" })
+    table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
+    table.insert(res, round_end)
+  end
+
+  if symbol.implementation then
+    if #res > 0 then
+      table.insert(res, { " ", "NonText" })
+    end
+    table.insert(res, round_start)
+    table.insert(res, { "󰡱 ", "SymbolUsageImpl" })
+    table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
+    table.insert(res, round_end)
+  end
+
+  return res
+end
+
+-- Setup symbolusage
+require("symbol-usage").setup({
+  text_format = text_format,
+})
+
+-- Setup Linters
+require("lint").linters_by_ft = {
+  lua = { "selene" },
+}
+
+-- configure nvim-linter
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
+
+require("formatter").setup({
+  filetype = {
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+    },
+  },
+})
+
+-- Format on save
+vim.api.nvim_create_augroup("__formatter__", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = "__formatter__",
+  command = ":FormatWrite",
+})
+
+-- set indentation options
+-- For filetype-specific configuration create a file named ftplugin/<file-type>.lua and set options there
+local o = vim.o
+
+o.expandtab = true -- expand tab input with spaces characters
+o.smartindent = true -- syntax aware indentations for newline inserts
+o.tabstop = 2 -- num of space characters per tab
+o.shiftwidth = 2 -- spaces per indentation level
+
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    -- { name = "vsnip" }, -- For vsnip users.
+    { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = "buffer" },
+  }),
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+  sources = cmp.config.sources({
+    { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+  }, {
+    { name = "buffer" },
+  }),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+})
+
+-- Set up lspconfig.
+require("cmp_nvim_lsp").default_capabilities()
+
+-- Language Server Protocol (LSP) Configuration
+
+require("lspconfig").lua_ls.setup({
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace",
+      },
+    },
+  },
+})
+
+require("lspconfig").jsonls.setup({
+  settings = {
+    json = {
+      schemas = require("schemastore").json.schemas(), -- use SchemaStore to get autocompletion in a wide set of .json files
+      validate = { enable = true },
+    },
+  },
+})
+
+require("lspconfig").yamlls.setup({
+  settings = {
+    yaml = {
+      schemaStore = {
+        -- You must disable built-in schemaStore support if you want to use
+        -- this plugin and its advanced options like `ignore`.
+        enable = false,
+        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+        url = "",
+      },
+      schemas = require("schemastore").yaml.schemas(), -- use SchemaStore to get autocompletion in a wide set of .yaml/.yml files
+    },
+  },
+})
