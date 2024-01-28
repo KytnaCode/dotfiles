8ee26be983ffdccc57a416c6c0728e4f91ef5929
Kytnacode <kytnacode@gmail.com>
2024-01-27 17:38:05 -0700

    Add aerial.nvim

--- init.lua
+++ init.lua
@@ -76,6 +76,25 @@ require("lazy").setup({
   "b0o/schemastore.nvim",
   "nvim-treesitter/nvim-treesitter",
   {
+    "stevearc/aerial.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("aerial").setup({
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        end,
+      })
+    end,
+  },
+  {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
@@ -200,6 +219,9 @@ end)
 vim.keymap.set("n", "gR", function()
   require("trouble").toggle("lsp_references")
 end)
+
+-- Aerial keymaps
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 
 -- LSP Keymaps
 vim.keymap.set({ "n", "i" }, "<C-space>", vim.lsp.buf.code_action)
